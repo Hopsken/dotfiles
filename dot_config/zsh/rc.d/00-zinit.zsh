1b6218b8d30ee75a27aaa41eecf5753c97baeed3
Hopsken <shaoweime@gmail.com>
2026-06-08 16:34:43 +0800

    feat: add pnpm to mise config

--- dot_config/zsh/rc.d/00-zinit.zsh
+++ dot_config/zsh/rc.d/00-zinit.zsh
@@ -1,7 +1,7 @@
 #####################
 # ZINIT             #
 #####################
-ZINIT_HOME="${XDG_DATA_HOME:-${HOME}/.local/share}/zinit/zinit.git"
+ZINIT_HOME="$XDG_DATA_HOME/zinit/zinit.git"
 [ ! -d $ZINIT_HOME ] && mkdir -p "$(dirname $ZINIT_HOME)"
 [ ! -d $ZINIT_HOME/.git ] && git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
 source "${ZINIT_HOME}/zinit.zsh"
